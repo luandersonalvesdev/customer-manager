cc0f8b0a875d43fa6ee7688797132331e4d1e5ef
Luanderson Alves <luaoderson@gmail.com>
2024-03-15 20:15:29 -0300

    test: backend: unit controller get all costumers

--- backend/tests/unit/controller/customer.test.ts
+++ backend/tests/unit/controller/customer.test.ts
@@ -5,14 +5,14 @@ import { Request, Response } from 'express';
 import CustomerService from '../../../src/services/CustomerService';
 import CustomerController from '../../../src/controllers/CustomerController';
 import {
-  CUSTOMER_FORM_MOCK, SUCCESS_RESPONSE_CREATED_CUSTOMER_MOCK, SUCCESS_CUSTOMER_CREATED_MOCK
+  CUSTOMER_FORM_MOCK, SUCCESS_RESPONSE_CREATED_CUSTOMER_MOCK, SUCCESS_RESPONSE_GET_ALL_CUSTOMERS_MOCK
 } from '../../mocks/customer.mock';
 
 describe('Customer Controller Unit Tests', () => {
 
   afterEach(() => sinon.restore());
 
-  it('Should create a new user', async () => {
+  it('Should create a new customer', async () => {
     sinon.stub(CustomerService.prototype, 'createCustomer').resolves(SUCCESS_RESPONSE_CREATED_CUSTOMER_MOCK as any);
     const req: Partial<Request> = { body: CUSTOMER_FORM_MOCK };
     const res: Partial<Response> = {
@@ -29,5 +29,24 @@ describe('Customer Controller Unit Tests', () => {
 
     expect((res.json as sinon.SinonStub).calledOnce).to.be.true;
     expect((res.json as sinon.SinonStub).calledWith(SUCCESS_RESPONSE_CREATED_CUSTOMER_MOCK.data)).to.be.true;
+  });
+
+  it('Should get all customers', async () => {
+    sinon.stub(CustomerService.prototype, 'getAllCustomers').resolves(SUCCESS_RESPONSE_GET_ALL_CUSTOMERS_MOCK as any);
+    const req: Partial<Request> = { query: { limit: "10", offset: "0" } };
+    const res: Partial<Response> = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const customerController = new CustomerController();
+
+    await customerController.getAllCustomers(req as Request, res as Response);
+
+    expect((res.status as sinon.SinonStub).calledOnce).to.be.true;
+    expect((res.status as sinon.SinonStub).calledWith(200)).to.be.true;
+
+    expect((res.json as sinon.SinonStub).calledOnce).to.be.true;
+    expect((res.json as sinon.SinonStub).calledWith(SUCCESS_RESPONSE_GET_ALL_CUSTOMERS_MOCK.data)).to.be.true;
   });
 });
