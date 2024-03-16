f229d009e6bb3493649b95c60b32fd7bec38d3c2
Luanderson Alves <luaoderson@gmail.com>
2024-03-16 05:25:52 -0300

    css: loading spinner added

---
+++ frontend/src/animations/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+export default function LoadingSpinner({ className = '' }) {
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={ `animate-spin rounded-full border-t-2
+        border-opacity-50 h-6 w-6 ${className}` }
+      />
+    </div>
+  );
+}
