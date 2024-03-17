d284bd80c5bee368f27c229f687571d75611906c
Luanderson Alves <luaoderson@gmail.com>
2024-03-16 23:41:40 -0300

    test: frontend: customers list done

--- frontend/src/__tests__/mocks/customer.mock.ts
+++ frontend/src/__tests__/mocks/customer.mock.ts
@@ -4,6 +4,12 @@ const VALID_CPF_MOCK = '11111111111';
 const VALID_PHONE_NUMBER_MOCK = '11111111111';
 const STATUS_MOCK = { id: 1, name: 'Ativo' }
 
+const VALID_FULL_NAME_2_MOCK = 'Lara';
+const VALID_EMAIL_2_MOCK = 'lara@email.com';
+const VALID_CPF_2_MOCK = '22222222222';
+const VALID_PHONE_NUMBER_2_MOCK = '22222222222';
+const STATUS_2_MOCK = { id: 2, name: 'Inativo' }
+
 export const CUSTOMER_MOCK = {
   id: 1,
   fullName: VALID_FULL_NAME_MOCK,
@@ -15,6 +21,23 @@ export const CUSTOMER_MOCK = {
   updatedAt: new Date(),
   status: STATUS_MOCK
 }
+
+export const CUSTOMER_2_MOCK = {
+  id: 2,
+  fullName: VALID_FULL_NAME_2_MOCK,
+  email: VALID_EMAIL_2_MOCK,
+  cpf: VALID_CPF_2_MOCK,
+  phoneNumber: VALID_PHONE_NUMBER_2_MOCK,
+  statusId: 2,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  status: STATUS_2_MOCK
+}
+
+export const CUSTOMERS_LIST_MOCK = [
+  CUSTOMER_MOCK,
+  CUSTOMER_2_MOCK
+]
 
 export const CUSTOMERS_WITHOUT_STATUS_MOCK = {
   ...CUSTOMER_MOCK,
---
+++ frontend/src/__tests__/unit/CustomersList.test.tsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import CustomersList from '../../components/CustomersList';
+import { BrowserRouter } from "react-router-dom";
+import { CUSTOMERS_LIST_MOCK } from "../mocks/customer.mock";
+
+
+describe("Unit - Customer Card Component", () => {
+  it("Should render the customer passed to it", () => {
+    render(
+      <BrowserRouter>
+        <CustomersList customers={CUSTOMERS_LIST_MOCK}/>
+      </BrowserRouter>
+    );
+
+    const customerNameText = screen.getByText('Jorel');
+    const customerEmailText = screen.getByText('jorel@email.com');
+    const customerCpfText = screen.getByText('111.111.111-11');
+    const customerPhoneNumberText = screen.getByText('(11) 11111-1111');
+    const statusText = screen.getByText('Ativo');
+
+    expect(customerNameText).toBeInTheDocument();
+    expect(customerEmailText).toBeInTheDocument();
+    expect(customerCpfText).toBeInTheDocument();
+    expect(customerPhoneNumberText).toBeInTheDocument();
+    expect(statusText).toBeInTheDocument();
+
+    const customerNameText2 = screen.getByText('Lara');
+    const customerEmailText2 = screen.getByText('lara@email.com');
+    const customerCpfText2 = screen.getByText('222.222.222-22');
+    const customerPhoneNumberText2 = screen.getByText('(22) 22222-2222');
+    const statusText2 = screen.getByText('Inativo');
+
+    expect(customerNameText2).toBeInTheDocument();
+    expect(customerEmailText2).toBeInTheDocument();
+    expect(customerCpfText2).toBeInTheDocument();
+    expect(customerPhoneNumberText2).toBeInTheDocument();
+    expect(statusText2).toBeInTheDocument();
+
+    const editLinks = screen.getAllByRole('link', { name: 'Editar' });
+
+    expect(editLinks).toHaveLength(2);
+  });
+})
