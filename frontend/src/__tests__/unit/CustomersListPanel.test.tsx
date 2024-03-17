1fa40c9bd33730c8661d37278b7b025533ef08a1
Luanderson Alves <luaoderson@gmail.com>
2024-03-17 20:13:28 -0300

    test: frontend: customers list panel

---
+++ frontend/src/__tests__/unit/CustomersListPanel.test.tsx
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom';
+import { render, waitFor } from '@testing-library/react';
+import CustomerListPanel from '../../pages/Dashboard/CustomersListChild/CustomersListPanel';
+import { BrowserRouter } from "react-router-dom";
+import CustomerService from '../../services/CustomerService';
+import { CUSTOMERS_LIST_MOCK } from '../mocks/customer.mock';
+
+describe("Unit - Customers List Panel", () => {
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
+  it("Should make fetch of all customers", async () => {
+    const getAllCustomersMock = jest.spyOn(CustomerService.prototype, 'getAllCustomers').mockResolvedValue(CUSTOMERS_LIST_MOCK);
+    render(
+      <BrowserRouter>
+        <CustomerListPanel />
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      expect(getAllCustomersMock).toHaveBeenCalled();
+    });
+  });
+
+  it("Should not make fetch of all customers", async () => {
+    const setCustomers = jest.fn();
+    try {
+      jest.spyOn(CustomerService.prototype, 'getAllCustomers').mockRejectedValue({});
+    } catch (error) {
+      expect(setCustomers).toHaveBeenCalled();
+    }
+    render(
+      <BrowserRouter>
+        <CustomerListPanel />
+      </BrowserRouter>
+    );
+  });
+})
